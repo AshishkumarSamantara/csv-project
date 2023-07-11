a613d8fa005a770ebf60ef439b20f3d26f80f7a1
AshishkumarSamantara <132549546+AshishkumarSamantara@users.noreply.github.com>
2023-07-11 18:16:38 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,35 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
+// mongoose.set('strictQuery',true);
+// const dotenv = require('.env');
+const DB = 'mongodb+srv://pawarsubham438:6aO4JOECgoI7Xu71@cluster0.bemtdzm.mongodb.net/mernstack?retryWrites=true&w=majority';
+// dotenv.config({ path: 'config/.env' });
 
-mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+//connecting mongoose with database
+//I stored MONGODB_URI in my system veriable for security reason. veriable name MONGODB_URI followed by your mongo atlas link
+//for local use you can write this code
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/csvUploader');
+
+// mongoose.connect(DB, {
+// 	useNewUrlParser: true,
+// 	useCreateIndex:true,
+//  	useUnifiedTopology: true,
+// 	useFindAndModify:false
+// }).then(() => {
+// 	console.log('connection successful');
+// }).catch((err) => console.log('no connection',err));
+
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.once('open', function(){
-    console.log('Data base connect to the server successfully!!!');
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
 });
 
-module.export = db;
+module.exports = mongoose;
